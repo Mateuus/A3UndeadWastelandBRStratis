9cf269d602a631ff6103d2d6314a45495f5f0a5f
Mateuus <mateuus27@outlook.com>
2016-06-19 04:21:39 -0300

    Update

---
+++ server.bat
@@ -0,0 +1,7 @@
+Start bec.bat 
+
+arma3server.exe  -port=2302 "-config=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\TADST\wastelandStratis\TADST_config.cfg" "-cfg=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\TADST\wastelandStratis\TADST_basic.cfg" "-profiles=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\TADST\wastelandStratis" -name=wastelandStratis -filePatching "-mod=@extDB2;@infiSTAR_A3"
+
+echo ARMA 3 Server has started.
+
+exit
